801cda1b31460547b9225d05ec0f11d363578d3b
Marek <forgame2312@interia.pl>
2020-11-05 00:39:34 +0100

    Pobieranie danych z bazy

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Controllers
+{
+    // Post http://localhost:5000/api/Values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+         //   return new string[] { "value1", "value2", "value3", "value4" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult AddValue([FromBody] Value value)
+        {
+            _context.Values.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult EditValue(int id, [FromBody] Value value)
+        {
+            var data= _context.Values.Find(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            _context.SaveChanges();
+            return Ok(data);
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteValue(int id)
+        {
+            var data = _context.Values.Find(id);
+            _context.Values.Remove(data);
+            _context.SaveChanges();
+            return Ok(data);
+
+        }
+    }
+}
